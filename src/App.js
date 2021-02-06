803f3eff12c2eb53823aee3493e4a0822d97fcd4
Ignacio <dev.ignacio.h@gmail.com>
2021-02-06 18:51:20 -0300

    state para formulario desde componente padre app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { Fragment } from 'react';
 import Formulario from './components/Formulario';
 import Header from './components/Header';
+import { useState } from 'react';
 
 function App() {
+  // Definir el state
+  // cantidad tendrá el valor que sea el state
+  // guardarCantidad es la función para guardar o modificar el state
+  const [cantidad, guardarCantidad] = useState(0);
+
   return (
     <Fragment>
       <Header titulo="Cotizador de Préstamos" />
       <div className="container">
-        <Formulario />
+        <Formulario cantidad={cantidad} guardarCantidad={guardarCantidad} />
       </div>
     </Fragment>
   );
